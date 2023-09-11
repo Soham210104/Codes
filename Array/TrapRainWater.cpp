c400e76708381fe8d436be422ddf06409fd09b53
Soham Ovhal <ovhalsoham@gmail.com>
2023-09-12 01:31:59 +0530

    Update TrapRainWater.cpp

--- Array/TrapRainWater.cpp
+++ Array/TrapRainWater.cpp
@@ -1,3 +1,7 @@
+// Given an array arr[] of N non-negative integers representing the height of blocks.
+//  If width of each block is 1, compute how much water can be trapped between the blocks during the rainy season. 
+
+
 class Solution{
 
     // Function to find the trapped water between the blocks.
