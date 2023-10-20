22d68ad9759865cd871ed0fa0a5420d1d6c7b9e1
Soham Ovhal <ovhalsoham@gmail.com>
2023-10-20 19:38:18 +0530

    Practice code

---
+++ Sorting/MergeThreeSortedArrays.cpp
@@ -0,0 +1,28 @@
+class Solution{
+  public:
+    // A, B, and C: input sorted arrays
+    //Function to merge three sorted vectors or arrays 
+    //into a single vector or array.
+    vector<int> mergeThree(vector<int>& A, vector<int>& B, vector<int>& C) 
+    { 
+        //Your code here
+        vector<int> v;
+        
+        for(int i=0;i<A.size();i++)
+        {
+            v.push_back(A[i]);
+        }
+        for(int i=0;i<B.size();i++)
+        {
+            v.push_back(B[i]);
+        }
+        for(int i=0;i<C.size();i++)
+        {
+            v.push_back(C[i]);
+        }
+        
+        sort(v.begin(),v.end());
+        return v;
+    } 
+
+};
