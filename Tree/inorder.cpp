355740e3891ec7f6d01281fe1ed1e9e05cb1d18b
Soham Ovhal <ovhalsoham@gmail.com>
2024-02-05 17:10:59 +0530

    Traversals

---
+++ Tree/inorder.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+  
+    void solve(Node* root,vector<int>& ans)
+    {
+        if(root == NULL)return;
+        
+        solve(root->left,ans);
+        ans.push_back(root->data);
+        solve(root->right,ans);
+    }
+    // Function to return a list containing the inorder traversal of the tree.
+    vector<int> inOrder(Node* root) 
+    {
+        // Your code here
+        vector<int> ans;
+        solve(root,ans);
+        
+        return ans;
+    }
+};
