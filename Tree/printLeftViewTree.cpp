110c993775062b57a2344b2d3847553d16d5a7ae
Soham Ovhal <ovhalsoham@gmail.com>
2024-02-13 19:11:34 +0530

    printLeftView

---
+++ Tree/printLeftViewTree.cpp
@@ -0,0 +1,36 @@
+vector<int> leftView(Node *root)
+{
+   // Your code here
+   vector<int> v;
+   queue<Node*> q;
+   q.push(root);
+   if(root == NULL)
+   {
+       return v;
+   }
+   while(!q.empty())
+   {
+       int count = q.size();
+       for(int i=0;i<count;i++)
+       {
+           Node* curr  = q.front();
+           q.pop();
+           
+           if(i==0)
+           {
+               v.push_back(curr->data);
+           }
+           if(curr->left != NULL)
+           {
+               q.push(curr->left);
+           }
+           if(curr->right != NULL)
+           {
+               q.push(curr->right);
+           }
+           
+       }
+       
+   }
+   return v;
+}
