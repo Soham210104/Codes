98ba6dcb3bc6484cbde964cdbd39273c5e7c3397
Soham Ovhal <ovhalsoham@gmail.com>
2024-02-22 10:58:30 +0530

    binaryTreetoCDLL

---
+++ Tree/binaryTreetoCDLL.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+  public:
+    //Function to convert binary tree into circular doubly linked list.
+    Node* prev = NULL;
+    Node* head = NULL;
+    Node *bTreeToCList(Node *root)
+    {
+        if(root == NULL) return NULL;
+        
+        bTreeToCList(root->left);
+        if(head == NULL)
+        {
+            head = root;
+        }
+        
+        root->left = prev;
+        if(prev!=NULL)
+        {
+            prev->right = root;
+        }
+        
+        prev = root;
+        
+        
+        bTreeToCList(root->right);
+        if(root->right==NULL)
+        {
+            root->right = head;
+            head->left = root;
+        }
+        return head;
+        
+        
+    }
+};
