1514ff0dc2c480e246ffd99b532ede52a3d8a7c1
Soham Ovhal <ovhalsoham@gmail.com>
2024-06-20 16:23:55 +0530

    COMPARATOR
    lambda comparator

---
+++ Revision/Array/LargestNumberFormedFromArray.cpp
@@ -0,0 +1,29 @@
+class Solution{
+public:
+	// The main function that returns the arrangement with the largest value as
+	// string.
+	// The function accepts a vector of strings
+	string printLargest(int n, vector<string> &arr) {
+	    // code here
+	    
+	    
+	    //s1+s2 > s2+s1 return true means while sorting if s1+s2 > s2+s1 then keep s1 before s2 else s2 before s1.
+	    auto cmp = [](string &s1,string &s2){
+	      if(s1+s2 > s2+s1){
+	          return true;
+	      } 
+	      return false;
+	    };
+	    
+	    sort(arr.begin(),arr.end(),cmp);
+	    
+	    string result = " ";
+	    
+	    
+	    for(string &s : arr){
+	        result += s;
+	    }
+	    
+	    return result;
+	}
+};
