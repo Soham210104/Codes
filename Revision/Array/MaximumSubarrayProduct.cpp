1e3e135ab45ae4fdc9679477986b83d32cae3432
Soham Ovhal <ovhalsoham@gmail.com>
2024-08-13 16:58:14 +0530

    PRACTICE

--- Revision/Array/MaximumSubarrayProduct.cpp
+++ Revision/Array/MaximumSubarrayProduct.cpp
@@ -19,3 +19,34 @@ public:
 	    return maxP;
 	}
 };
+
+//OPTIMAL SOLUTION
+// class Solution{
+// public:
+
+// 	// Function to find maximum product subarray
+// 	long long maxProduct(vector<int> arr, int n) {
+// 	    // code here
+// 	    if(n==1) return arr[0];
+// 	    long long product = 1,product1=1;
+// 	    long long maxP = INT_MIN;
+	    
+// 	    for(int i=0;i<n;i++){
+// 	        product = product * arr[i];
+// 	        product1 = product1 * arr[n-i-1];
+// 	        maxP = max(maxP,max(product,product1));
+	        
+// 	        if(arr[i] == 0){
+// 	            product = 1;
+// 	           // continue;
+// 	        }
+// 	        if(arr[n-i-1] == 0){
+// 	            product1 = 1;
+// 	        }
+// 	    }
+	    
+	    
+	    
+// 	    return maxP;
+// 	}
+// };
